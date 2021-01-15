a0c5e8ee419e8889539ffa971b1e5ce90dcacb1e
AC <83970162@qq.com>
2021-01-15 16:33:54 +0800

    drag and drop support (#1)
    * drag and drop support
    
    * change the version of cdk, and remove useless code
    
    * change version of cdk
    
    * update version

---
+++ src/app/draggable/drag-basic/drag-basic.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ColumnMode } from 'projects/swimlane/ngx-datatable/src/public-api';
+
+@Component({
+  selector: 'app-drag-basic',
+  templateUrl: './drag-basic.component.html',
+  styleUrls: ['./drag-basic.component.scss']
+})
+export class DragBasicComponent implements OnInit {
+  dataSource: any[] = [];
+  ColumnMode = ColumnMode;
+
+  constructor() {
+    this.fetch(data => {
+      this.dataSource = data.map((item, index) => ({ ...item, id: index }));
+      // this.convertData(data);
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onRowDrag(event) {
+    const {
+      item: { data },
+      previousIndex,
+      currentIndex
+    } = event;
+    const dataSource = [...this.dataSource];
+    const step = currentIndex - previousIndex; // moved steps
+    const index = dataSource.findIndex(d => d.id === data.id);
+    const temp = dataSource.splice(index, 1);
+    dataSource.splice(index + step, 0, ...temp);
+    this.dataSource = dataSource;
+  }
+
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/data/forRowGrouping.json`);
+
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+
+    req.send();
+  }
+}
